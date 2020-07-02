0bc59ece2b37a7cc908096261a6f143e20b65140
Chislaine Cruz <chislaine.cruz@gmail.com>
2020-07-02 14:44:32 -0400

    feat: added ground and yeti

--- public/game.js
+++ public/game.js
@@ -3,13 +3,13 @@
 
 var config = {
   type: Phaser.AUTO,
-  width: 400,
-  height: 300,
+  width: 1200,
+  height: 700,
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
-      debug: false,
+      gravity: { y: 800 },
+      debug: true,
     },
   },
   scene: {
@@ -21,31 +21,43 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-// const game = new Phaser.Game(800, 600, Phaser.AUTO, "", {
-//   preload: preload,
-//   create: create,
-//   update: update,
-// });
-
 let player;
 
 function preload() {
-  // game.load.image("sky", "./assets/sky.png");
-  this.load.image("sky", "assets/sky.png");
-  this.load.spritesheet("kirby", "./assets/kirby.png", {
-    frameWidth: 32,
-    frameHeight: 32,
+  this.load.image("background", "./assets/background.png");
+  this.load.spritesheet("yeti", "./assets/yeti.png", {
+    frameWidth: 62,
+    frameHeight: 62,
   });
-  // game.load.spritesheet("dude", "./assets/dude.png", 32, 32);
+  this.load.spritesheet("husky", "./assets/husky.png", {
+    frameWidth: 62,
+    frameHeight: 62,
+  });
+  this.load.spritesheet("tiles", "./assets/tiles.png", {
+    frameWidth: 100,
+    frameHeight: 60,
+  });
 }
 function create() {
-  // this.physics.startSystem(Phaser.Physics.ARCADE);
-  this.add.image(0, 0, "sky");
-  player = this.physics.add.sprite(100, 450, "kirby");
-  // game.physics.arcade.enable(player);
+  let bg = this.add.sprite(0, 200, "background");
+  bg.setOrigin(0, 0);
+  bg.setScale(1.6);
+  //creates 7 ground blocks that are the width of the block. 1 is for the height
+  //the first 2 nums are the position on the screen
+  let ground = this.add.tileSprite(500, 650, 7 * 100, 1 * 60, "tiles");
+  // the true parameter makes the ground static
+  this.physics.add.existing(ground, true);
+
+  ground.body.allowGravity = false;
+  ground.body.immovable = true;
+
+  player = this.physics.add.sprite(200, 450, "yeti");
   player.body.bounce.y = 0.2;
   player.body.gravity.y = 800;
   player.body.collideWorldBounds = true;
+
+  //makes the player and ground collide
+  this.physics.add.collider(ground, player);
 }
 
 function update() {}
